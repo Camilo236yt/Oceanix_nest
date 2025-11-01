e22fa78e9269fe13a8c60ac1158628caf28fc7e6
Camilo236YT <93947024+Camilo236yt@users.noreply.github.com>
2025-11-01 16:07:48 -0500

    feature:Se Agrego login-dtos, register.-dtos  se hizo condifuracion base de datosfeat(auth): refactor authentication module and add database integration
    - Added @nestjs/config and @nestjs/typeorm for configuration and database support.
    - Implemented TypeORM with PostgreSQL in app.module.ts.
    - Replaced CreateAuthDto and UpdateAuthDto with LoginDto and RegisterDto.
    - Updated AuthController to handle login and registration.
    - Removed unused DTO files.

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,11 +8,47 @@ import { RolesModule } from './roles/roles.module';
 import { PermissionsModule } from './permissions/permissions.module';
 import { MessagesModule } from './messages/messages.module';
 import { NotificationModule } from './notification/notification.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 
 @Module({
-  imports: [IncidenciasModule, AuthModule, UsersModule, ClientsModule, EnterpriseModule, RolesModule, PermissionsModule, MessagesModule, NotificationModule],
+  imports: [
+
+ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+
+      TypeOrmModule.forRootAsync({
+
+        useFactory: () => ({
+          type: 'postgres',
+          host: process.env.DB_host,
+          port: 5432,
+          username: process.env.DB_user,
+          password: process.env.DB_password,
+          database: process.env.DB_name,
+          autoLoadEntities: true,
+          synchronize: true,
+        }),
+    }),
+     IncidenciasModule,
+     AuthModule,
+     UsersModule,
+     ClientsModule,
+     EnterpriseModule,
+     RolesModule,
+     PermissionsModule,
+     MessagesModule,
+     NotificationModule
+  ],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule {
+
+
+  constructor() {    console.log('Database Host:', process.env.DB_host);
+  }
+}
