824d7f33297c37bd92d4f06ecf9ea629c55dca2c
Camilo236YT <93947024+Camilo236yt@users.noreply.github.com>
2025-11-10 09:58:34 -0500

    Feature: Entity create

--- src/incidencias/entities/incidencia.entity.ts
+++ src/incidencias/entities/incidencia.entity.ts
@@ -1,1 +1,22 @@
-export class Incidencia {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+@Entity('incidencias')
+export class Incidencia {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+    @Column()
+    name: string;
+    @Column()
+    description: string;
+    @Column()
+    status: string;
+    @Column()
+    createdAt: Date;
+    @Column()
+    updatedAt: Date;
+    @Column()
+    photoUrl?: string;
+    @Column()
+    tenantId: string;
+}
+
+
--- src/incidencias/incidencias.module.ts
+++ src/incidencias/incidencias.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { IncidenciasService } from './incidencias.service';
 import { IncidenciasController } from './incidencias.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Incidencia } from './entities/incidencia.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Incidencia])],
   controllers: [IncidenciasController],
   providers: [IncidenciasService],
 })
